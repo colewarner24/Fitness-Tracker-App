b5e608646152936b312b18d24dab74308f8d68eb
Z_ <zxcvbbvvf2@gmail.com>
2024-11-06 11:16:14 -0600

    updated ProgressMapper.java and ProgressMapper.xml

--- src/main/java/fitnesstracker/mapper/ProgressMapper.java
+++ src/main/java/fitnesstracker/mapper/ProgressMapper.java
@@ -1,6 +1,59 @@
 package fitnesstracker.mapper;
 
-import fitnesstracker.model.Progress;
+import java.util.List;
 
-public interface ProgressMapper extends BaseMapper<Progress, Integer> {
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import fitnesstracker.model.Progress;
+import fitnesstracker.util.MyBatisUtil;
+
+public class ProgressMapper implements BaseMapper<Progress, Integer> {
+    public static final SqlSessionFactory factory = MyBatisUtil.getSqlSessionFactory();
+
+    @Override
+    public void create(Progress progress) {
+        try (SqlSession session = factory.openSession()) {
+            session.insert("fitnesstracker.mapper.ProgressMapper.create", progress);
+            session.commit();
+        }
+    }
+
+    @Override
+    public Progress findById(Integer id) {
+        try (SqlSession session = factory.openSession()) {
+            return session.selectOne("fitnesstracker.mapper.ProgressMapper.findById", id);
+        }
+    }
+
+    @Override
+    public List<Progress> getAll() {
+        try (SqlSession session = factory.openSession()) {
+            return session.selectList("fitnesstracker.mapper.ProgressMapper.getAll");
+        }
+    }
+
+    @Override
+    public void update(Progress progress) {
+        try (SqlSession session = factory.openSession()) {
+            session.update("fitnesstracker.mapper.ProgressMapper.update", progress);
+            session.commit();
+        }
+    }
+
+    @Override
+    public void delete(Integer id) {
+        try (SqlSession session = factory.openSession()) {
+            session.delete("fitnesstracker.mapper.ProgressMapper.delete", id);
+            session.commit();
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        try (SqlSession session = factory.openSession()) {
+            session.delete("fitnesstracker.mapper.ProgressMapper.deleteAll");
+            session.commit();
+        }
+    }
 }
